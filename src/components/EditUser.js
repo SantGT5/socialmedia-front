0ad035b888f1e87ab1bc6e920d9624a351883732
GianLucas <gianspf@gmail.com>
2021-08-16 15:26:42 +0200

    GianLucas

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -9,11 +9,11 @@ import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import EmailIcon from "@material-ui/icons/Email";
 
-import InputLabel from "@material-ui/core/InputLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
+// import InputLabel from "@material-ui/core/InputLabel";
+// import FormHelperText from "@material-ui/core/FormHelperText";
+// import FormControl from "@material-ui/core/FormControl";
+// import Select from "@material-ui/core/Select";
+// import MenuItem from "@material-ui/core/MenuItem";
 import React from "react";
 
 import Dialog from "@material-ui/core/Dialog";
