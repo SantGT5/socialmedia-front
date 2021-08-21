79a49423e16013d3ba95f3744e1987621299f5ce
GianLucas <gianspf@gmail.com>
2021-08-22 01:37:10 +0200

    GianLucas - Profile

--- src/components/GlobalCard.js
+++ src/components/GlobalCard.js
@@ -15,6 +15,8 @@ import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+import img from "../img/Sem Título.png"
+
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
@@ -26,7 +28,7 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function RecipeReviewCard() {
+export default function RecipeReviewCard(props) {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -47,21 +49,19 @@ export default function RecipeReviewCard() {
           </IconButton>
         }
         title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        subheader={ props.addLocation }
       />
       <CardMedia
         sx={{
           height: 0,
           paddingTop: '56.25%', // 16:9
         }}
-        image="/static/images/cards/paella.jpg"
-        title="Paella dish"
+        src={ props.postImgURL }
+        component="img"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          { props.description }
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -71,6 +71,11 @@ export default function RecipeReviewCard() {
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
+        <div className="d-flex justify-content-sm-end">
+        <span >Tag people</span>
+        </div>
+        
+        {/* style={{ marginLeft:"13.9em", width:"5.1em" }} */}
         <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
@@ -82,30 +87,8 @@ export default function RecipeReviewCard() {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+            { props.tagUser }
           </Typography>
         </CardContent>
       </Collapse>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,73 @@
-import NavBar from "./NavBar"
+import NavBar from "./NavBar";
+import GlobalCard from "./GlobalCard";
+import api from "../apis/api";
 
-import GlobalCard from "./GlobalCard"
+import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
 
-function Profile(){
-return(
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
+
+function Profile() {
+const classes = useStyles();
+const history = useHistory()
+  const [status, setStatus] = useState([]);
+
+
+
+
+  const handleClick = () => {
+    history.push("/newpost")
+  }
+
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        const response = await api.get("/allpost");
+        console.log("response -> ", response);
+        setStatus([ ...response.data ]);
+      } catch (err) {
+        console.log(err.response);
+      }
+    }
+    fetchProfile();
+  }, []);
+
+  return (
     <div>
-
-<NavBar />
-<div>
-
-
+      <NavBar />
+      <div className="allPost d-flex flex-column-reverse bd-highlight">
+        {status.map((elem, i) => {
+          return (
+            <div key={i}  style={{ width:"100vw", maxWidth:"33em", marginBottom:"1em" }} className="p-2 bd-highlight">
+              <GlobalCard
+                // title={elem.profileName}
+                addLocation={elem.addLocation}
+                postImgURL={elem.postImgURL}
+                description={elem.description}
+                tagUser={elem.tagUser}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div id="IconHome" className={classes.root}>
+        <Fab onClick={handleClick} color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </div>
     </div>
-    </div>
-)
-
+  );
 }
 
-export default  Profile
+export default Profile;
